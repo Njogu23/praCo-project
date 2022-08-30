ba140e62b62ec92f9197e374265d08da1b600fcd
George Njogu <sbeveme@gmail.com>
2022-08-30 18:26:23 +0300

    structuring cs

---
+++ index.js
@@ -0,0 +1,20 @@
+// const foo = document.getElementsByClassName('section');
+
+// function func(item) {
+//     item.addEventListener('mouseover',() => item.style.fontSize = '50px')
+    
+// }
+
+// func()
+
+// function fn() {
+//     foo.forEach(func(foo))
+// }
+
+// fn()
+
+// // function fn() {
+// //     foo.addEventListener('mouseout', () => foo.style.fontSize= '45px')
+// // }
+
+// // fn() 

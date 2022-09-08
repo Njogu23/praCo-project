af27ed82c7151068d196961d5389a09372fc9226
George Njogu <sbeveme@gmail.com>
2022-09-09 02:44:45 +0300

    finalizing

--- index.js
+++ index.js
@@ -2,68 +2,73 @@ const response = fetch('https://exercisedb.p.rapidapi.com/exercises', {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-		'X-RapidAPI-Key': 'd595c4331bmsh78067501c0d5056p1a9a6ajsnc6dd3a85e9a6',
+		'X-RapidAPI-Key': '86332af65amsha50a1827694ecaep1dece6jsnfa56659d3b85',
 	}
 }).then(res => res.json())
+
+const bodyTargets = fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+		'X-RapidAPI-Key': '86332af65amsha50a1827694ecaep1dece6jsnfa56659d3b85',
+	}
+}).then(res => res.json())
+
 
 const searchInput = document.querySelector('#text')
 const rootDiv = document.getElementById('root')
 
 
 document.addEventListener("DOMContentLoaded", () => {
-	document.querySelector('form').addEventListener('submit', (e) => {
-	 e.preventDefault();
-	 searchItems(searchInput.value)
+	const createDivs = () => {
+		
+		bodyTargets.then(data => data.forEach(items => {
+			const target = document.createElement('a')
+			const rootDiv = document.createElement('div')
+			
+			rootDiv.style.display = 'grid'
+			rootDiv.style.justifyContent = 'center'
+			rootDiv.appendChild(target)
+			const main = document.querySelector('#body')
+			main.appendChild(rootDiv)
+			target.textContent = items
+			target.style.background = 'grey'
+			target.style.padding = '15px'
+			target.style.margin = '2px'
+			target.style.width = '300px'
+			target.style.cursor = 'pointer'
 	
-   })
-
-
-
-const searchItems = (target) => {
+			target.addEventListener('mouseover', () => 
+			target.style.background = '#D3D3D3' )
 	
-	response.then(data => data.filter(element => {
-		if (element.bodyPart === target) {
-			const parentDiv = document.createElement('div')
-	    const gifURL = document.createElement('img')
-	    const workoutName = document.createElement('p')
-		workoutName.style.fontSize = '17px'
-		gifURL.src = element.gifUrl
-		workoutName.textContent = `workout: ${element.name}`
-		parentDiv.appendChild(gifURL)
-	    parentDiv.appendChild(workoutName)
-	    return rootDiv.appendChild(parentDiv)
+			target.addEventListener('mouseout', () => 
+			target.style.background = 'gray')
+
+			target.addEventListener('click', () => {
+				response.then(data => data.filter(el => {
+					if(el.bodyPart === target.textContent) {
+						const div = document.createElement('div')
+						const gifURL = document.createElement('img')
+			            const workoutName = document.createElement('p')
+						gifURL.src = el.gifUrl
+						workoutName.textContent = `workout : ${el.name}`
+						div.appendChild(gifURL)
+						div.appendChild(workoutName)
+						rootDiv.appendChild(div)
+
+					}
+				}))
+			})
+
+			
+		} ))
+		
+	
 	}
-
-	       
-	}
-	))
+	createDivs()
 	
-	}
-	
-})
-
-const createDivs = () => {
-	// const btn = document.getElementsByClassName('bodyParts') 
-	fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-			'X-RapidAPI-Key': 'd595c4331bmsh78067501c0d5056p1a9a6ajsnc6dd3a85e9a6',
-		}
-	}).then(res => res.json())
-	.then(data => data.forEach(items => {
-		const target = document.createElement('a')
-		const main = document.querySelector('#body')
-		main.appendChild(target)
-		target.textContent = items
-		target.style.background = 'grey'
-		target.style.padding = '15px'
-		target.style.margin = '2px'
-	    target.style.width = '300px'
-		target.style.cursor = 'pointer'
-
-	} ))
-	
-
 }
-createDivs()
+)
+
+
+

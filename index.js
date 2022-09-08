4673e10c2093ff2c2750770a417ce17b2fa046ac
George Njogu <sbeveme@gmail.com>
2022-09-09 00:05:37 +0300

    adding tergets

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const searchInput = document.querySelector('#text');
-const searchBtn = document.getElementById('search');
+const response = fetch('https://exercisedb.p.rapidapi.com/exercises', {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+		'X-RapidAPI-Key': 'd595c4331bmsh78067501c0d5056p1a9a6ajsnc6dd3a85e9a6',
+	}
+}).then(res => res.json())
+
+const searchInput = document.querySelector('#text')
 const rootDiv = document.getElementById('root')
 
 
@@ -10,36 +17,53 @@ document.addEventListener("DOMContentLoaded", () => {
 	
    })
 
-const response = fetch('https://exercisedb.p.rapidapi.com/exercises', {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-		'X-RapidAPI-Key': 'd595c4331bmsh78067501c0d5056p1a9a6ajsnc6dd3a85e9a6',
-	}
-}).then(res => res.json())
-
 
 
 const searchItems = (target) => {
 	
 	response.then(data => data.filter(element => {
-		if (element.bodyPart === target)
-
-		{const parentDiv = document.createElement('div')
+		if (element.bodyPart === target) {
+			const parentDiv = document.createElement('div')
 	    const gifURL = document.createElement('img')
 	    const workoutName = document.createElement('p')
+		workoutName.style.fontSize = '17px'
 		gifURL.src = element.gifUrl
 		workoutName.textContent = `workout: ${element.name}`
-
 		parentDiv.appendChild(gifURL)
 	    parentDiv.appendChild(workoutName)
 	    return rootDiv.appendChild(parentDiv)
 	}
 
-	    
+	       
 	}
 	))
 	
 	}
 	
 })
+
+const createDivs = () => {
+	// const btn = document.getElementsByClassName('bodyParts') 
+	fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+			'X-RapidAPI-Key': 'd595c4331bmsh78067501c0d5056p1a9a6ajsnc6dd3a85e9a6',
+		}
+	}).then(res => res.json())
+	.then(data => data.forEach(items => {
+		const target = document.createElement('a')
+		const main = document.querySelector('#body')
+		main.appendChild(target)
+		target.textContent = items
+		target.style.background = 'grey'
+		target.style.padding = '15px'
+		target.style.margin = '2px'
+	    target.style.width = '300px'
+		target.style.cursor = 'pointer'
+
+	} ))
+	
+
+}
+createDivs()
